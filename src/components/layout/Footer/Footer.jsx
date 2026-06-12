9f598213455aa96a66b3aeeaefb2f0c84259bc28
Fedya Styopochkin <fst20071221@gmail.com>
2026-06-12 14:05:53 +0000

    feat: add text for footer

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './footer.module.css';
 
 export default function Footer() {
   return (
-    <footer>
-      <p>Footer</p>
+    <footer className={styles.footer}>
+      © 2026 ShopCart. Your favorite place for smart shopping. All rights reserved.
     </footer>
   );
 }
