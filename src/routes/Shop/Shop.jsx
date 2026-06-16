e63d180fcf70e2765f7925587bf74a42eb0bc376
Fedya Styopochkin <fst20071221@gmail.com>
2026-06-16 13:12:34 +0000

    fix: delete redundant styles and enhance responsiveness in SearchInput

--- src/routes/Shop/Shop.jsx
+++ src/routes/Shop/Shop.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import SearchInput from '../../components/SearchInput/SearchInput';
 
 export default function Shop() {
-  return <></>;
+  return <SearchInput placeholder="Search for products" />;
 }
