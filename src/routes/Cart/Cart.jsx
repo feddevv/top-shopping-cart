64549072ef32087a42d619a7fe9973327d05a396
Fedya Styopochkin <fst20071221@gmail.com>
2026-06-17 10:37:17 +0000

    feat: add order summary details

--- src/routes/Cart/Cart.jsx
+++ src/routes/Cart/Cart.jsx
@@ -1,9 +1,15 @@
 import { useOutletContext } from 'react-router';
 import styles from './cart.module.css';
 import Card from '../../components/Card/Card';
+import Button from '../../components/Button/Button';
 
 export default function Cart() {
   const { cart } = useOutletContext();
+
+  const summary = {
+    totalPrice: cart.reduce((acc, curr) => acc + curr.price, 0),
+    totalProducts: cart.length,
+  };
 
   return (
     <main className={styles.main}>
@@ -24,6 +30,18 @@ export default function Cart() {
 
       <section className={styles.summary}>
         <h2>Order Summary</h2>
+        <div className={styles['summary-details']}>
+          <p>
+            Total price: <span>${summary.totalPrice}</span>
+          </p>
+          <p>
+            Total amount: <span>{summary.totalProducts}</span>
+          </p>
+
+          <div className={styles.divider}></div>
+
+          <Button type={'primary'}>Order Now</Button>
+        </div>
       </section>
     </main>
   );
